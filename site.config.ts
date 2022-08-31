986345795160d380c90357121532b5513647eafa
pavstar619 <pavelx16@gmail.com>
2022-08-31 07:14:22 +0600

    sriji init

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'c7400c54a1ac4267b9634040bf7a59fb',
+  rootNotionPageId: '9542e0ccebf6481f8094d038c611b761',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,18 +10,18 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
-  domain: 'pavel-portfolio.vercel.app',
-  author: 'Pavel Rahman',
+  domain: 'susmita-portfolio.vercel.app',
+  author: 'susmita Rahman',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'pavstar619',
-  github: 'pavstar619',
-  linkedin: 'pavel-rahman',
+  // twitter: 'pavstar619',
+  github: 'susmita-debnath',
+  linkedin: 'susmita-debnath-6577b3231',
   // newsletter: '#', // optional newsletter URL
-  youtube: 'c/DevPav', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  // youtube: 'c/DevPav', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
